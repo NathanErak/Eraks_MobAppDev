8a86c6f80c5ac626087004ec000831522939ccf4
NathanErak <85259693+NathanErak@users.noreply.github.com>
2023-10-25 11:21:05 -0600

    Add files via upload

---
+++ POO_PM_2/src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+import Clases.National
+import Clases.NationalLowSeason
+import Clases.Travel
+
+fun main() {
+    //val travel = Travel
+    val viajeMonterrey = National("Monterrey")
+    viajeMonterrey.quotePrice(2)
+    val viajeBajaMonterrey = NationalLowSeason("Monterrey")
+}
